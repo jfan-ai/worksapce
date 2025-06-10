0769462e3f70d4104b0252b8ed134a55e5786a6f
jfan_ai <jfan08559@gmail.com>
2025-06-10 15:07:56 +0800

    123

--- webook2/internal/web/init_web.go
+++ webook2/internal/web/init_web.go
@@ -1,9 +1,24 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 func RegisterRoutes() *gin.Engine {
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{"PUT", "POST", "GET", "OPTIONS"},
+		AllowHeaders: []string{"authorization", "content-type"},
+		//ExposeHeaders:    []string{},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	registerUsersRoutes(server)
 	return server
 }
